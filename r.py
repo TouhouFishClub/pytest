acf87dbd47dac3d554d42cf6ffde2edd8e999adb
ter <a26214311@gmail.com>
2018-12-03 13:06:23 +0800

    weather

--- r.py
+++ r.py
@@ -11,7 +11,18 @@ def getWeather():
 	req = session.get(url)
 	soup = BeautifulSoup(req.text,'html.parser')
 	date = soup.find(attrs={'class':'op_weather4_twoicon_date'}).get_text().strip()
-	print(date)
+	tempNumber = soup.find(attrs={'class':'op_weather4_twoicon_shishi_title'}).get_text().strip()
+	tempSup = soup.find(attrs={'class':'op_weather4_twoicon_shishi_sup'}).get_text().strip()
+	tempSub = soup.find(attrs={'class':'op_weather4_twoicon_shishi_sub'}).get_text().strip()
+	dayTemp = soup.find(attrs={'class':'op_weather4_twoicon_temp'}).get_text().strip()
+	dayWea = soup.find(attrs={'class':'op_weather4_twoicon_weath'}).get_text().strip()
+	dayWind = soup.find(attrs={'class':'op_weather4_twoicon_wind'}).get_text().strip()
+	dayQuailty = soup.find(attrs={'class':'op_weather4_twoicon_aqi_level_4_bg op_weather4_twoicon_realtime_quality_today'}).span.get_text().strip()
+	weatherReportNow = date + "\n" + tempNumber + tempSup + "  " + tempSub + "\n"
+	weatherReportDay = date + "\n" + dayTemp + "  " + dayWea + "," + dayWind + u",空气质量指数" + dayQuailty
+
+	print(weatherReportNow)
+	print(weatherReportDay)
 
 
 getWeather()
