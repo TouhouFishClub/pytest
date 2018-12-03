d996f1428606cfa06684df76a01829bd176f53a5
Flandre <flantan233@gmail.com>
2018-12-03 14:28:16 +0800

    update

--- r.py
+++ r.py
@@ -1,28 +1,43 @@
 #-*-encoding:utf-8 -*-
 import requests
 from bs4 import BeautifulSoup
+import sys
+
 
 
 session = requests.Session()
 
 # 获取参数tid
-def getWeather():
-	url = 'http://www.baidu.com/s?wd=%E5%8C%97%E4%BA%AC%E5%A4%A9%E6%B0%94'
-	req = session.get(url)
-	soup = BeautifulSoup(req.text,'html.parser')
-	date = soup.find(attrs={'class':'op_weather4_twoicon_date'}).get_text().strip()
-	tempNumber = soup.find(attrs={'class':'op_weather4_twoicon_shishi_title'}).get_text().strip()
-	tempSup = soup.find(attrs={'class':'op_weather4_twoicon_shishi_sup'}).get_text().strip()
-	tempSub = soup.find(attrs={'class':'op_weather4_twoicon_shishi_sub'}).get_text().strip()
-	dayTemp = soup.find(attrs={'class':'op_weather4_twoicon_temp'}).get_text().strip()
-	dayWea = soup.find(attrs={'class':'op_weather4_twoicon_weath'}).get_text().strip()
-	dayWind = soup.find(attrs={'class':'op_weather4_twoicon_wind'}).get_text().strip()
-	dayQuailty = soup.find(attrs={'class':'op_weather4_twoicon_realtime_quality_wrap'}).span.span.get_text().strip()
-	weatherReportNow = date + "\n" + tempNumber + tempSup + "  " + tempSub + "\n"
-	weatherReportDay = date + "\n" + dayTemp + "  " + dayWea + "," + dayWind + u",空气质量指数" + dayQuailty
+def getWeather(area):
+	try:
+		url = 'http://www.baidu.com/s?wd=' + area + '天气'
+		req = session.get(url)
+		soup = BeautifulSoup(req.text,'html.parser')
+		date = soup.find(attrs={'class':'op_weather4_twoicon_date'}).get_text().strip()
+		print('【' + area + '天气】')
 
-	print(weatherReportNow)
-	print(weatherReportDay)
+		try:
+			tempNumber = soup.find(attrs={'class':'op_weather4_twoicon_shishi_title'}).get_text().strip()
+			tempSup = soup.find(attrs={'class':'op_weather4_twoicon_shishi_sup'}).get_text().strip()
+			tempSub = soup.find(attrs={'class':'op_weather4_twoicon_shishi_sub'}).get_text().strip()
+			weatherReportNow = date + "\n" + tempNumber + tempSup + "  " + tempSub
+			print(weatherReportNow)
+		except:
+			pass
+
+		dayTemp = soup.find(attrs={'class':'op_weather4_twoicon_temp'}).get_text().strip()
+		dayWea = soup.find(attrs={'class':'op_weather4_twoicon_weath'}).get_text().strip()
+		dayWind = soup.find(attrs={'class':'op_weather4_twoicon_wind'}).get_text().strip()
+		try:
+			dayQuailty = soup.find(attrs={'class':'op_weather4_twoicon_realtime_quality_wrap'}).span.span.get_text().strip()
+		except:
+			dayQuailty = ''
+		weatherReportDay = date + "\n" + dayTemp + "  " + dayWea + "," + dayWind + u",空气质量指数" + dayQuailty
+		print(weatherReportDay)
+		print('')
+	except:
+		print('失败')
 
 
-getWeather()
+for i in range(1, len(sys.argv)):
+	getWeather(sys.argv[i])
